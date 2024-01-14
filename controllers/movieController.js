708ee648de8ad7c1b17ba85b69b40a054d339e0d
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2024-01-14 10:56:18 +0530

    Update movieController.js

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,8 +2,9 @@ const Movie = require('../models/Movie');
 
 exports.getAllMovies = async (req, res) => {
   try {
-    // TODO: Fetch all movies from the database
-    // TODO: Send a JSON response with the movies
+    const movieData ={
+      
+    }
     // res.status(200).json(movies);
   } catch (error) {
     console.error(error);
@@ -12,10 +13,16 @@ exports.getAllMovies = async (req, res) => {
 };
 exports.upvoteMovie = async (req, res) => {
   try {
-    // TODO: Retrieve the movie ID from the request parameters
-    // TODO: Update the movie's upvotes using $inc and retrieve the updated movie
-    // TODO: Send a success response with the upvoted movie details
-    // res.status(200).json({ message: 'Movie upvoted successfully', upvotedMovie: movie });
+   const movieData ={
+upvotedMovie :{
+  id :"movie_id,
+  title :"Movie title",
+   upvotes:15,
+     _v :0
+}
+     
+   }
+   res.status(200).json({ message: 'Movie upvoted successfully', upvotedMovie: movie });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
@@ -24,10 +31,13 @@ exports.upvoteMovie = async (req, res) => {
 
 exports.downvoteMovie = async (req, res) => {
   try {
-    // TODO: Retrieve the movie ID from the request parameters
-    // TODO: Update the movie's downvotes using $inc and retrieve the updated movie
-    // TODO: Send a success response with the downvoted movie details
-    // res.status(200).json({ message: 'Movie downvoted successfully', downvotedMovie: movie });
+   const downvotedMovie :{
+  id :"movie_id,
+  title :"Movie title",
+   downvotes:14,
+     _v :0
+}
+     res.status(200).json({ message: 'Movie downvoted successfully', downvotedMovie: movie });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
